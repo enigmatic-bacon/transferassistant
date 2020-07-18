ae58f5fdfece61f35dd0d5d0bbcb56b5356f9d75
RuoqingVeronicaGan <ganruoqing1995@gmail.com>
2020-07-18 01:50:11 -0700

    test commit

--- lib/main.dart
+++ lib/main.dart
@@ -98,7 +98,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'HI I LOVE YOU:',
+              'HI I LOVE YOU TOo',
             ),
             Text(
               '$_counter',
