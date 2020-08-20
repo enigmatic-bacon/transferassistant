54a06507b4a5adf15f31abc0767896a91b71956e
AlexBismuth <bismuthalex@gmail.com>
2020-08-20 16:14:33 -0700

    moved things out of page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'backEnd/universityList.dart';
+import 'hello_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -20,43 +19,6 @@ class MyApp extends StatelessWidget {
           child: MyStatefulWidget(),
         ),
       ),
-    );
-  }
-}
-
-class MyStatefulWidget extends StatefulWidget {
-  MyStatefulWidget({Key key}) : super(key: key);
-
-  @override
-  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
-}
-
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  String dropdownValue = 'WyoTech';
-
-  @override
-  Widget build(BuildContext context) {
-    return DropdownButton<String>(
-      value: dropdownValue,
-      icon: Icon(Icons.cake),
-      iconSize: 24,
-      elevation: 16,
-      style: TextStyle(color: Colors.deepOrange),
-      underline: Container(
-        height: 2,
-        color: Colors.deepOrangeAccent,
-      ),
-      onChanged: (String newValue) {
-        setState(() {
-          dropdownValue = newValue;
-        });
-      },
-      items: getUniversityList().map<DropdownMenuItem<String>>((String value) {
-        return DropdownMenuItem<String>(
-          value: value,
-          child: Text(value),
-        );
-      }).toList(),
     );
   }
 }
