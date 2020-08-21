c63ecde6eda65bc6016e3a305a650b1c994aa66f
AlexBismuth <bismuthalex@gmail.com>
2020-08-20 17:08:20 -0700

    Added text for front page

--- lib/collegeListPage.dart
+++ lib/collegeListPage.dart
@@ -3,24 +3,47 @@ import 'package:flutter/material.dart';
 class CollegeListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          FlatButton(
-            onPressed: () {
-              Navigator.pushNamed(context, '/second');
-            },
-            child: Text('go to taskPage'),
-          ),
-          FlatButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            child: Text('go back to helloPage'),
-          ),
-        ],
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: RichText(
+                text: TextSpan(
+                  style: TextStyle(fontSize: 20.0, color: Colors.red),
+                  children: <TextSpan>[
+                    TextSpan(
+                      text: 'Hello Me!\n',
+                      style: TextStyle(
+                          fontSize: 36.0,
+                          color: Colors.black,
+                          fontWeight: FontWeight.w900),
+                    ),
+                    TextSpan(
+                      text: 'To which college are you being accepted to today?',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            FlatButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/second');
+              },
+              child: Text('go to taskPage'),
+            ),
+            FlatButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('go back to helloPage'),
+            ),
+          ],
+        ),
       ),
     );
   }
