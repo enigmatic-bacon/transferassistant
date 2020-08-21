9d72ed11323fbe1b67d3f886968cc7a3f991d652
AlexBismuth <bismuthalex@gmail.com>
2020-08-20 17:43:25 -0700

    added autocomplete text field

--- lib/helloPage.dart
+++ lib/helloPage.dart
@@ -1,28 +1,64 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'backEnd/universityList.dart';
+import 'package:autocomplete_textfield/autocomplete_textfield.dart';
 
 class HelloPage extends StatefulWidget {
   HelloPage({Key key}) : super(key: key);
-
   @override
   _HelloPageState createState() => _HelloPageState();
 }
 
 class _HelloPageState extends State<HelloPage> {
   String dropdownValue = 'WyoTech';
-
+  SimpleAutoCompleteTextField textField;
+  GlobalKey<AutoCompleteTextFieldState<String>> textFieldKey = new GlobalKey();
+  List<String> added = [];
+  String currentText = "";
+  List<String> suggestions = [
+    "Apple",
+    "Armidillo",
+    "Actual",
+    "Actuary",
+    "America",
+    "Argentina",
+    "Australia",
+    "Antarctica",
+    "Blueberry",
+    "Cheese",
+    "Danish",
+    "Eclair",
+    "Fudge",
+    "Granola",
+    "Hazelnut",
+    "Ice Cream",
+    "Jely",
+    "Kiwi Fruit",
+    "Lamb",
+    "Macadamia",
+    "Nachos",
+    "Oatmeal",
+    "Palm Oil",
+    "Quail",
+    "Rabbit",
+    "Salad",
+    "T-Bone Steak",
+    "Urid Dal",
+    "Vanilla",
+    "Waffles",
+    "Yam",
+    "Zest"
+  ];
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: RichText(
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              RichText(
                 text: TextSpan(
                   style: TextStyle(fontSize: 20.0, color: Colors.red),
                   children: <TextSpan>[
@@ -34,20 +70,35 @@ class _HelloPageState extends State<HelloPage> {
                           fontWeight: FontWeight.w900),
                     ),
                     TextSpan(
-                      text: 'To which college are you being accepted to today?',
+                      text: 'Today I want to be accepted to: ',
                       style: TextStyle(fontWeight: FontWeight.bold),
                     ),
                   ],
                 ),
               ),
-            ),
-            FlatButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/first');
-              },
-              child: Text('Go to College List Page'),
-            )
-          ],
+              SimpleAutoCompleteTextField(
+                key: textFieldKey,
+                controller: TextEditingController(text: null),
+                suggestions: suggestions,
+                textChanged: (text) => currentText = text,
+                textSubmitted: (text) {
+                  setState(() {
+                    if (text.length != 0) {
+                      suggestions.remove(text);
+                      added.add(text);
+                      print(added);
+                    }
+                  });
+                },
+              ),
+              FlatButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/first');
+                },
+                child: Text('Go to College List Page'),
+              )
+            ],
+          ),
         ),
       ),
     );
